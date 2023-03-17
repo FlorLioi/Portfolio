46d10cbb2b76040ad294004bc0db4e36a7244cd0
florencia <flor_camilal@hotmail.com>
2023-03-16 22:37:20 -0300

    Agrego responsive

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-// let check = document.querySelector('.check');
-// check.addEventListener('click', idioma);
-
-// function idioma(){
-//     let id = check.checked;
-//     if(id==true){
-//         location.href='en/index.html';
-//     }else{
-//         location.href='../index.html';
-//     }
-// }
-
 const toggleMenuElement = document.getElementById('toggle-menu');
 const mainMenuElement = document.getElementById('main-menu');
 
