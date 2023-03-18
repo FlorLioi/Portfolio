0c3fa0b34eef7d1bd10ba89c48c1d25317362613
florencia <flor_camilal@hotmail.com>
2023-03-18 18:10:06 -0300

    Agrego idioma

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
+/* NAV */
+
 const toggleMenuElement = document.getElementById('toggle-menu');
 const mainMenuElement = document.getElementById('main-menu');
 
 toggleMenuElement.addEventListener('click', () =>{
     mainMenuElement.classList.toggle('main-menu--show');
+});
+
+/* LANGUAGE */
+
+const flagsElement = document.getElementById('flags');
+const textsToChange = document.querySelectorAll('[data-section]');
+
+const changeLanguage = async language =>{
+    const requestJson = await fetch(`./languages/${language}.json`)
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        textToChange.innerHTML = texts[section][value];
+    }
+
+}
+
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
 });
