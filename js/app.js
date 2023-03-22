06a74f20edd0c57d22b10d245541b855fe5f19f4
florencia <flor_camilal@hotmail.com>
2023-03-21 21:01:46 -0300

    Nuevo diseño

--- js/app.js
+++ js/app.js
@@ -9,21 +9,22 @@ toggleMenuElement.addEventListener('click', () =>{
 
 /* LANGUAGE */
 
-const flagsElement = document.getElementById('flags');
-const textsToChange = document.querySelectorAll('[data-section]');
+const flagsElement = document.getElementById('flags');//Primero localizas el elemento
 
-const changeLanguage = async language =>{
-    const requestJson = await fetch(`./languages/${language}.json`)
-    const texts = await requestJson.json();
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+});//Para acceder a ese parametro (e.target.parentElement.dataset.language) 
 
-    for(const textToChange of textsToChange){
-        const section = textToChange.dataset.section;
-        const value = textToChange.dataset.value;
+const textsToChange = document.querySelectorAll('[data-section]'); //Localizas los textos
+
+const changeLanguage = async language =>{ //Funcion que lee el json
+    const requestJson = await fetch(`./languages/${language}.json`) //Pasamos la ruta
+    const texts = await requestJson.json(); //Lo convertimos en objeto y poder utilizarlo
+
+    for(const textToChange of textsToChange){ //Recorremos los textos
+        const section = textToChange.dataset.section; //Tomamos la section
+        const value = textToChange.dataset.value;// Tomamos el value
         textToChange.innerHTML = texts[section][value];
     }
 
 }
-
-flagsElement.addEventListener('click', (e) => {
-    changeLanguage(e.target.parentElement.dataset.language);
-});
